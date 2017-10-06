f7e52a429e0cda894ac4a51425d658b8e5a8b10b
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-10-06 08:53:05 -0600

    Added IBOutlet and IBAction to my view controller for my button

--- FirstSwiftApp/ViewController.swift
+++ FirstSwiftApp/ViewController.swift
@@ -11,19 +11,24 @@ import UIKit
 //The : means it inherits from
 class ViewController: UIViewController
 {
-
+    
+    @IBOutlet weak var PrettyButton: UIButton!
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func PrettyMethod(_ sender: UIButton)
+    {
+        
+    }
+    
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
