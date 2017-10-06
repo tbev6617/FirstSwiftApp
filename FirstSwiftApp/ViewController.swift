11ffeb1c93fa6a3e10b7bcdc8895cbb4f88ba678
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-10-06 08:42:08 -0600

    Made a pretty button

--- FirstSwiftApp/ViewController.swift
+++ FirstSwiftApp/ViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//The : means it inherits from
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
